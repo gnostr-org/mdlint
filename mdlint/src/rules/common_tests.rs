c23de09a56aad7df263891ae8515e88fe55a3784
randymcmillan <randymcmillan@protonmail.com>
2024-07-20 18:08:55 -0400

    mdlint/src/rules/common_tests.rs:update

--- mdlint/src/rules/common_tests.rs
+++ mdlint/src/rules/common_tests.rs
@@ -3,7 +3,7 @@ use crate::parser::get_ast;
 use crate::ruleset::CheckFn;
 use typed_arena::Arena;
 
-crate fn all_ok(file: &str, check: CheckFn) {
+pub fn all_ok(file: &str, check: CheckFn) {
     let arena = Arena::new();
     let a = &arena;
     let root = get_ast(file, a);
